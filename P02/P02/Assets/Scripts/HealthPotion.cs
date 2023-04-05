b8e163c30b45601921eb93d41fb1a30ae8277d7e
Andrea Passini <andreapa98@gmail.com>
2023-04-05 16:53:02 +0200

    VR

--- P02/P02/Assets/Scripts/HealthPotion.cs
+++ P02/P02/Assets/Scripts/HealthPotion.cs
@@ -1,50 +1,50 @@
 ﻿using UnityEngine;
 
 namespace DefaultNamespace {
-    public class HealthPotion: MonoBehaviour, IHittable, IPotion
+public class HealthPotion: MonoBehaviour, IHittable, IPotion
+{
+    #region C# Event Health Potion Hit
+        public delegate void OnHealthPotionHit(float gainedArrows);
+        public static event OnHealthPotionHit onHealthPotionHit;
+    #endregion
+
+    [SerializeField] private GameObject vfxHealthExplosion;
+    [SerializeField] private AudioClip sfxHealthExplosion;
+    public float destroyEffectsDuration = 5f;
+    
+    private int _gainedArrows = 10;
+    
+    public void Hit(Transform hitter)
     {
-        #region C# Event Health Potion Hit
-            public delegate void OnHealthPotionHit(float gainedArrows);
-            public static event OnHealthPotionHit onHealthPotionHit;
-        #endregion
-
-        [SerializeField] private GameObject vfxHealthExplosion;
-        [SerializeField] private AudioClip sfxHealthExplosion;
-        public float destroyEffectsDuration = 5f;
-        
-        private int _gainedArrows = 10;
-        
-        public void Hit(Transform hitter)
-        {
-            onHealthPotionHit?.Invoke(_gainedArrows);
-            DestroyPotion(hitter, vfxHealthExplosion, sfxHealthExplosion, destroyEffectsDuration);
-        }
-        
-        public static void InvokeArrowEvent(int numberOfArrows)
-        {
-            onHealthPotionHit?.Invoke(numberOfArrows);
-        }
-
-        public void DestroyPotion(Transform hitter, GameObject vfx, AudioClip sfx, float fxDuration)
-        {
-            // Destroy arrow
-            if(hitter != null)
-                Destroy(hitter.gameObject);
-            
-            // Destroy vfx
-            GameObject vfxInstantiated;
-            vfxInstantiated = Instantiate(
-                vfx,
-                transform.position,
-                transform.rotation
-            );
-            Destroy(vfxInstantiated, fxDuration);
-            
-            // Destroy sfx
-            SoundManager.Instance.PlayClip(sfx);
-            
-            // Destroy Potion
-            Destroy(gameObject);
-        }
+        onHealthPotionHit?.Invoke(_gainedArrows);
+        DestroyPotion(hitter, vfxHealthExplosion, sfxHealthExplosion, destroyEffectsDuration);
     }
+    
+    public static void InvokeArrowEvent(int numberOfArrows)
+    {
+        onHealthPotionHit?.Invoke(numberOfArrows);
+    }
+
+    public void DestroyPotion(Transform hitter, GameObject vfx, AudioClip sfx, float fxDuration)
+    {
+        // Destroy arrow
+        if(hitter != null)
+            Destroy(hitter.gameObject);
+        
+        // Destroy vfx
+        GameObject vfxInstantiated;
+        vfxInstantiated = Instantiate(
+            vfx,
+            transform.position,
+            transform.rotation
+        );
+        Destroy(vfxInstantiated, fxDuration);
+        
+        // Destroy sfx
+        SoundManager.Instance.PlayClip(sfx);
+        
+        // Destroy Potion
+        Destroy(gameObject);
+    }
+}
 }
---
+++ P02/P02/Assets/Scripts/WallArrowInteraction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class WallArrowInteraction: MonoBehaviour, IHittable
+    {
+        public void Hit(Transform hit)
+        {
+            // Catch the arrow
+        }
+    }
+}
